b06cde63d303aecfd7a52c304ecb928ebe7d7df1
Daniel Garcia <lequodo@gmail.com>
2021-04-22 13:01:18 +0200

    Olvide las migraciones xD

---
+++ database/migrations/2015_04_16_180121_create_user_permissions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_role_id');
+            $table->string("permiso_ruta");
+
+            $table->foreign("user_role_id")->references("id")->on("user_roles");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_permissions');
+    }
+}
