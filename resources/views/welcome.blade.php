b121e88c1875d2628b5234294775aa32f91bf80c
Daniel Garcia <lequodo@gmail.com>
2021-06-03 20:40:57 +0200

    Maquetar wellcome3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,26 +10,17 @@
         <div class="col-12 col-md-6">
             <img class="img-fluid" id="refugioGif" src="{{url(asset("storage/web/Animal shelter.gif"))}}" alt="Animación de una ilustración de un refugio de mascotas">
         </div>
-        <div class="col-12 col-md-6">
-            <div class="wellcome__container">
-                <div class="">
-                    <div class="">
-                        <p>El lugar donde puedes encontrar a tu mascota</p>
-                    </div>
-                </div>
-                <div class="">
-                    <div class="">
-                        <p>¿A qué esperas? Entra a mirar y busca la que mejor se adapte a tus circustancias.</p>
-                    </div>
-                </div>
-                <div class="">
-                    <a href="{{ route("mascotas") }}" class="btn btn-petfy-inverse">ENTRAR</a>
-                </div>
-
+        <div class="col-12 col-md-6 justify-content-around d-flex flex-column">
+            <div class="welcome__texto">
+                <p>El lugar donde puedes encontrar a tu mascota</p>
+            </div>
+            <div class="welcome__texto">
+                <p>¿A qué esperas? Entra a mirar y busca la que mejor se adapte a tus circustancias.</p>
+            </div>
+            <div class="text-center welcome__texto">
+                <a href="{{ route("mascotas") }}" class="btn btn-petfy-inverse">ENTRAR</a>
             </div>
         </div>
-
-
     </div>
     <script>
         let refugio =  document.getElementById("refugioGif");
