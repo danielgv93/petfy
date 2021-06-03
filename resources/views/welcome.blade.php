a211e36c5af7d08e8d06752428cdd0d5840f7695
Daniel Garcia <lequodo@gmail.com>
2021-06-03 20:11:43 +0200

    Maquetar wellcome2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,18 +11,18 @@
             <img class="img-fluid" id="refugioGif" src="{{url(asset("storage/web/Animal shelter.gif"))}}" alt="Animación de una ilustración de un refugio de mascotas">
         </div>
         <div class="col-12 col-md-6">
-            <div class="d-flex align-items-center justify-content-center flex-column ">
-                <div class="row">
-                    <div class="col-12">
+            <div class="wellcome__container">
+                <div class="">
+                    <div class="">
                         <p>El lugar donde puedes encontrar a tu mascota</p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12">
+                <div class="">
+                    <div class="">
                         <p>¿A qué esperas? Entra a mirar y busca la que mejor se adapte a tus circustancias.</p>
                     </div>
                 </div>
-                <div class="row">
+                <div class="">
                     <a href="{{ route("mascotas") }}" class="btn btn-petfy-inverse">ENTRAR</a>
                 </div>
 
